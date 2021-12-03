1eb422e6adc3f514fe34521013b5a284bd4deac6
yanxiao <2230261758@qq.com>
2021-12-03 15:18:02 +0800

    删除多余文件

--- vue-demo/src/utils/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Description:
- * @Author: John Holl
- * @Github: https://github.com/hzylyh
- * @Date: 2021-06-15 08:46:10
- * @LastEditors: John Holl
- * @LastEditTime: 2021-06-15 08:46:11
- */
