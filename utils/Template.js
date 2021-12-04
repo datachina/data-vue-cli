16cf49345c216471c2dd0541e4f84c04ead8f7ae
yanxiao <2230261758@qq.com>
2021-12-04 14:49:10 +0800

    升级为1.10版本，可支持自定义模板

---
+++ utils/Template.js
@@ -0,0 +1,48 @@
+/*
+ * @Description:
+ * @Author: yanxiao
+ * @Github: https://github.com/yanxiaos
+ * @Date: 2021-12-04 11:10:09
+ * @LastEditors: yanxiao
+ */
+const fs = require('fs')
+module.exports = class Template {
+
+  static filePath = '../template.json'
+
+  // 获取模板文件，json数据
+  getTemplate () {
+    const tplObj = require(`${__dirname}/${Template.filePath}`)
+    return tplObj
+  }
+
+  // 返回模板名列表
+  getNameList () {
+    let tplObj = this.getTemplate()
+    return Object.keys(tplObj)
+  }
+
+  // 根据模板名称返回模板url
+  getTplUrl (tplName) {
+    const template = this.getTemplate()
+    return template[tplName]
+  }
+
+  // 新增模板，插入键值对，key存在则覆盖
+  add (key, val, callback) {
+    let tplObj = this.getTemplate()
+    tplObj[key] = val
+    fs.writeFile(`${__dirname}/../template.json`, JSON.stringify(tplObj), 'utf-8', callback)
+  }
+
+  // 删除模板，删除键值对，key不存在则返回错误
+  delete (key, callback) {
+    let tplObj = this.getTemplate()
+    if (!tplObj[key]) {
+      callback('模板不存在！')
+      return
+    }
+    delete tplObj[key]
+    fs.writeFile(`${__dirname}/../template.json`, JSON.stringify(tplObj), 'utf-8', callback)
+  }
+}
--- utils/downloadTemplate.js
+++ utils/downloadTemplate.js
@@ -1,4 +1,6 @@
-module.exports = function downloadTemplate ({ appName, url, branch }) {
+const fs = require('fs')
+
+module.exports = function downloadTemplate ({ appName, url }) {
   const program = require('commander')
   const chalk = require('chalk')
   const ora = require('ora')
@@ -11,30 +13,38 @@ module.exports = function downloadTemplate ({ appName, url, branch }) {
   spinner.start();
 
   // 执行下载方法并传入参数
-  try {
-    download(
-      `${url}#${branch}`,
-      appName,
-      { clone: true },
-      err => {
-        if (err) {
-          spinner.fail();
-          console.log(chalk.red(`下载失败！ ${err}`))
-          // 退出命令行
-          process.exit()
-          return
-        }
+  download(
+    url,
+    appName,
+    { clone: true },
+    err => {
+      if (err) {
+        spinner.fail();
+        console.log(chalk.red(`下载失败！ ${err}`))
+        // 退出命令行
+        process.exit()
+      }
+      setAppName(appName, (err) => {
+        console.log('errererere', err);
         // 结束加载图标
         spinner.succeed();
         console.log(chalk.green('\n 下载成功!'))
         console.log('\n To get started')
-        console.log(`\n    cd ${appName} \n`)
+        console.log(`\n    cd ${appName}`)
+        console.log(`    npm install \n`)
         // 退出命令行
         process.exit()
-      }
-    )
-  } catch (e) {
-    console.log('eeee', e);
-  }
+      })
+    }
+  )
+}
 
+
+// 将下载后的项目的package.json文件的name修改成永恒输入的appName
+function setAppName (appName, callback) {
+  let path = process.cwd()
+  const appPackage = require(`${path}/${appName}/package.json`)
+
+  appPackage.name = appName
+  fs.writeFile(`${path}/${appName}/package.json`, JSON.stringify(appPackage, null, '\t'), 'utf-8', callback)
 }
